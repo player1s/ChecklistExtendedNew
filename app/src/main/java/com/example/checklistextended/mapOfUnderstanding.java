02bb4e1f44b37303207b66313e2a79bed7425846
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-12-23 16:46:00 +0100

    First taste of the dragging functionality

--- app/src/main/java/com/example/checklistextended/mapOfUnderstanding.java
+++ app/src/main/java/com/example/checklistextended/mapOfUnderstanding.java
@@ -1,8 +1,13 @@
 package com.example.checklistextended;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Intent;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,6 +24,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 
 public class mapOfUnderstanding extends AppCompatActivity {
+
+    //---------------------------START: DRAGGING THINGS----------------------
+    ImageView img;
+    String msg;
+    private CoordinatorLayout.LayoutParams layoutParams;
+    //---------------------------END: DRAGGING THINGS----------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -84,7 +95,6 @@ public class mapOfUnderstanding extends AppCompatActivity {
                 CoordinatorLayout.LayoutParams lp = new CoordinatorLayout.LayoutParams(CoordinatorLayout.LayoutParams.WRAP_CONTENT, CoordinatorLayout.LayoutParams.WRAP_CONTENT);
 
                 // Add rule to layout parameters
-                // Add the ImageView below to Button
                 lp.setMargins(450,450,50,50);
 
                 // Add layout parameters to ImageView
@@ -95,8 +105,90 @@ public class mapOfUnderstanding extends AppCompatActivity {
             }
         });
 
+        //---------------------------START: DRAGGING THINGS FUNCTIONALITY----------------------
 
+        img=(ImageView)findViewById(R.id.my_image_view);
 
+        img.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData.Item item = new ClipData.Item((CharSequence)v.getTag());
+                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+
+                ClipData dragData = new ClipData(v.getTag().toString(),mimeTypes, item);
+                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(img);
+
+                v.startDrag(dragData,myShadow,null,0);
+                return true;
+            }
+        });
+
+        img.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                switch(event.getAction()) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        layoutParams = (CoordinatorLayout.LayoutParams)v.getLayoutParams();
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+
+                        // Do nothing
+                        break;
+
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
+                        int x_cord = (int) event.getX();
+                        int y_cord = (int) event.getY();
+                        break;
+
+                    case DragEvent.ACTION_DRAG_EXITED :
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
+                        x_cord = (int) event.getX();
+                        y_cord = (int) event.getY();
+                        layoutParams.leftMargin = x_cord;
+                        layoutParams.topMargin = y_cord;
+                        v.setLayoutParams(layoutParams);
+                        break;
+
+                    case DragEvent.ACTION_DRAG_LOCATION  :
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
+                        x_cord = (int) event.getX();
+                        y_cord = (int) event.getY();
+                        break;
+
+                    case DragEvent.ACTION_DRAG_ENDED   :
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
+
+                        // Do nothing
+                        break;
+
+                    case DragEvent.ACTION_DROP:
+                        Log.d(msg, "ACTION_DROP event");
+
+                        // Do nothing
+                        break;
+                    default: break;
+                }
+                return true;
+            }
+        });
+
+        img.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    ClipData data = ClipData.newPlainText("", "");
+                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(img);
+
+                    img.startDrag(data, shadowBuilder, img, 0);
+                    img.setVisibility(View.INVISIBLE);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        //---------------------------END: DRAGGING THINGS FUNCTIONALITY----------------------
     }
 
 }
