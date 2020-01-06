fc7c2d35d475c3802f0ab73e9067f81c17f61489
Levente Székely <31960469+player1s@users.noreply.github.com>
2020-01-06 12:22:32 +0100

    added recyclerview on timeline

--- app/src/main/java/com/example/checklistextended/timeLine.java
+++ app/src/main/java/com/example/checklistextended/timeLine.java
@@ -4,16 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class timeLine extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class timeLine extends AppCompatActivity implements mouRWAdapter.ItemClickListener{
+
+    mouRWAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timeline);
 
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAnimalsInTimeline);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new mouRWAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
 
         //set up the buttons on the toolbar
         Button btnTimeLineInTimeLine = (Button) findViewById(R.id.btnTimeLineInTimeLine);
@@ -49,6 +73,20 @@ public class timeLine extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final FloatingActionButton fabInTimeline = (FloatingActionButton) findViewById(R.id.fabInTimeline);
+        fabInTimeline.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getAdapter().newAddeddata("doesntmatter");
+            }
+        });
+    }
+    public mouRWAdapter getAdapter(){
+        return adapter;
     }
 
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
 }
